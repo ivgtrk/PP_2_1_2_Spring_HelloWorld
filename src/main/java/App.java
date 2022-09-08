057822c60b1a0ec44de025b79a12c81ba1304863
IgorVL <ivgtrk@hotmail.com>
2022-09-08 13:05:06 +0300

    Класс Cat добавлен. Бин настроен. Тест пройден!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HW 1 and 2 Equals?: " + bean.equals(bean2));
+        System.out.println("Cat 1 and 2 Equals?: " + cat1.equals(cat2));
     }
 }
